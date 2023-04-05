517c3a6a5bd0d31aa4ab38d021ce5581ff701df7
Fixfon <12512757+fixfon@users.noreply.github.com>
2023-04-05 20:02:09 +0300

    Created dummy ui and rpc routes

--- src/pages/dashboard/timetables.tsx
+++ src/pages/dashboard/timetables.tsx
@@ -1,9 +1,6 @@
-import { type NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-
-import { api } from "~/utils/api";
 
 const Home: NextPage = (props) => {
   return (
@@ -20,9 +17,9 @@ const Home: NextPage = (props) => {
           </h1>
           <div className="flex flex-col items-center justify-center gap-4 md:gap-8">
             <h3 className="text-xl font-bold text-white">
-              This is main landing page for signed in and not signed in users.
+              This is for viewing timetable pages (org admins and teachers can
+              access this).
             </h3>
-
             <div className="font-bold text-white">
               <h3 className="text-xl">Other Pages:</h3>
               <ul className="uppercase">
@@ -54,10 +51,6 @@ const Home: NextPage = (props) => {
               </ul>
             </div>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            {/* <CreateSuperAdmin /> */}
-            <AuthShowcase />
-          </div>
         </div>
       </main>
     </>
@@ -65,27 +58,3 @@ const Home: NextPage = (props) => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined }
-  // );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.id}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={
-          sessionData ? () => void signOut() : () => void signIn("credentials")
-        }
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
