99c21983caf1931fce04844ba7beff2203ac92b4
Fixfon <12512757+fixfon@users.noreply.github.com>
2023-05-30 18:07:18 +0300

    Added form component custom error msg

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -146,7 +146,11 @@ const FormMessage = React.forwardRef<
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, children, ...props }, ref) => {
   const { error, formMessageId } = useFormField();
-  const body = error ? String(error?.message) : children;
+  const body = error
+    ? children
+      ? children
+      : String(error?.message)
+    : children;
 
   if (!body) {
     return null;
